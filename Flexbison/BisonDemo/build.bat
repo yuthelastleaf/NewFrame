fb0ba24b82548b3a84405820789c449b0cfe1a3f
yulastleaf <yuthelastleaf@gmail.com>
2024-04-24 23:59:46 +0800

    fix : 修改了一下编译脚本，最初会因为符号解析编译失败，奇奇怪怪

---
+++ Flexbison/BisonDemo/build.bat
@@ -0,0 +1,5 @@
+cd %~dp0
+mkdir build
+cd build
+cmake -G "MinGW Makefiles" -DCMAKE_C_COMPILER="C:/msys64/mingw64/bin/gcc.exe" -DCMAKE_CXX_COMPILER="C:/msys64/mingw64/bin/g++.exe" ../
+cmake --build .
